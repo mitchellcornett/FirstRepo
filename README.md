e3fd3cb3e41b646212f16f920d813b044fcbef04
mitchellcornett <mitchell.cornett2@gmail.com>
2024-01-11 20:24:34 -0800

    Completed part 1 of javascript and added some html

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # FirstRepo
 The first repo I made
-
-Added web page starter project
--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+
+let numberOfWords = 3;
+
+let wordArray = [];
+
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
+    for (let i = 0; i < numberOfWords; i++) {
+
+        let wordEntered = prompt("Please enter a word:")
+
+        wordArray.push(wordEntered);
+    }
+
+    for (let i = 0; i < numberOfWords; i++) {
+
+        let li = document.getElementById("li" + i)
+
+        li.innerHTML = wordArray[i];
+    }
+
+    
 });
