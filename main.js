12316af758451fa34703d8fe27cbe5d893cf6f90
Marie <122072731+Mchuong10@users.noreply.github.com>
2024-01-11 21:25:53 -0800

    Update main.js
    added an alert if a word isn't entered.

--- main.js
+++ main.js
@@ -7,18 +7,21 @@ let wordArray = [];
 document.addEventListener("DOMContentLoaded", function (event) {
 
     for (let i = 0; i < numberOfWords; i++) {
-
-        let wordEntered = prompt("Please enter a word:")
-
+    
+        let wordEntered = prompt("Please enter a word:");
+        if (wordEntered === null || wordEntered.trim()=== ""){ 
+            alert("Please enter a valid word. Try Again");
+            i--;
+        } else { 
         wordArray.push(wordEntered);
     }
 
     for (let i = 0; i < numberOfWords; i++) {
 
         let li = document.getElementById("li" + i)
-
+        if (li){
         li.innerHTML = wordArray[i];
     }
-
+    }
     
 });
